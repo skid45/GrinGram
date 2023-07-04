4552b9064fe6c072f0c5ebd6f50d9fd65ef88fdd
skid45 <akopovgrisha@gmail.com>
2023-07-05 02:23:42 +0300

    Added the function of saving images from messages

--- app/src/main/java/com/skid/gringram/ui/FullscreenChatMediaFragment.kt
+++ app/src/main/java/com/skid/gringram/ui/FullscreenChatMediaFragment.kt
@@ -1,6 +1,12 @@
 package com.skid.gringram.ui
 
+import android.content.ContentValues
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,6 +20,8 @@ import com.skid.gringram.R
 import com.skid.gringram.databinding.FullscreenChatMediaBinding
 import com.skid.gringram.ui.adapter.SwipeMediaActionListener
 import com.skid.gringram.ui.adapter.SwipeMediaAdapter
+import com.squareup.picasso.Picasso
+import java.lang.Exception
 
 class FullscreenChatMediaFragment(
     private val media: List<String>,
@@ -60,6 +68,28 @@ class FullscreenChatMediaFragment(
                 setNavigationOnClickListener {
                     this@FullscreenChatMediaFragment.dismiss()
                 }
+                setOnMenuItemClickListener {
+                    if (it.itemId == R.id.save_image_item) {
+                        Picasso.get().load(media[fullscreenChatMediaPhotoViewPager.currentItem])
+                            .into(object : com.squareup.picasso.Target {
+                                override fun onBitmapLoaded(
+                                    bitmap: Bitmap?, from: Picasso.LoadedFrom?,
+                                ) {
+                                    if (bitmap != null) {
+                                        saveImageToGallery(bitmap)
+                                    }
+                                }
+
+                                override fun onBitmapFailed(
+                                    e: Exception?, errorDrawable: Drawable?,
+                                ) = Unit
+
+                                override fun onPrepareLoad(placeHolderDrawable: Drawable?) {}
+
+                            })
+                    }
+                    true
+                }
             }
 
             fullscreenChatMediaPhotoViewPager.apply {
@@ -83,6 +113,26 @@ class FullscreenChatMediaFragment(
             WindowManager.LayoutParams.MATCH_PARENT,
             WindowManager.LayoutParams.MATCH_PARENT
         )
+    }
+
+    private fun saveImageToGallery(bitmap: Bitmap) {
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, "image.jpg")
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+            }
+        }
+
+        val uri = requireActivity().contentResolver.insert(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues
+        )
+
+        uri?.let {
+            requireActivity().contentResolver.openOutputStream(it).use { outputStream ->
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+            }
+        }
     }
 
     companion object {
